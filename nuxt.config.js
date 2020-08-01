d9d75e2d67e26d310ed6cab2a9db3ecdad8787a6
MostefaMourad <hm_mostefa@esi.dz>
2020-08-01 02:46:55 +0100

    Cleaning the Project

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,18 +61,21 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      light: true,
       themes: {
         dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
+          background: '#00a86b'
+        },
+        light: {
+          background: '#d0f0c0',
+          primary: '#00adb5',
+          accent: '#222831',
           secondary: colors.amber.darken3,
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
           success: colors.green.accent3
         }
-      }
     }
   },
   /*
@@ -81,4 +84,5 @@ export default {
   */
   build: {
   }
+}
 }
