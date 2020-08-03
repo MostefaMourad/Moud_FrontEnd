a90cf6c88a8cbbdf9275006c2b3beb878fd5e622
MostefaMourad <hm_mostefa@esi.dz>
2020-08-03 15:15:48 +0100

    Home fix

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,8 @@
     align-center
   >
   <!---- Images Slider ------------->
+
+  <v-hover v-slot:default="{ hover }">
     <v-carousel
     cycle
     height="700"
@@ -12,14 +14,60 @@
     show-arrows-on-hover
   >
     <v-carousel-item
-      v-for="(slide, i) in slides"
+      v-for="(quote, i) in quotes"
       :key="i"
-      :src="items[i].src"
+      :src="file[i]"
     >
+        <v-overlay
+            v-if="hoverr"
+            absolute
+            opacity="0"
+            style="margin-left:-60%;margin-top:-20%;"
+          >
+             <v-card
+             flat
+             light
+             style="opacity:0.7;"
+             max-width="400"
+             >
+               <v-card-text>
+                 <v-row align="center" class="link">
+                   <v-col md="6">
+                     <img src="~/assets/accueil_6.png" style="opacity:1.0;" height="100" alt="">
+                   </v-col>
+                   <v-col md="6" class="qu" style="opacity:1.0;">
+                     commencer à gagner de l'argent
+                   </v-col>
+                 </v-row>
+               </v-card-text>
+               <v-divider :inset="inset"></v-divider>
+               <v-card-text>
+                 <v-row align="center" class="link">
+                   <v-col md="6">
+                     <img src="~/assets/accueil_5.png" style="opacity:1.0;" height="100" alt="">
+                   </v-col>
+                   <v-col md="6" class="qu" style="opacity:1.0;">
+                    accomplissez vos tâches et missions
+                   </v-col>
+                 </v-row>
+               </v-card-text>
+             </v-card>
+        </v-overlay>
+        <v-expand-transition>
+          <div
+            v-if="hover"
+            class="d-flex transition-fast-in-fast-out primary darken-4 v-card--reveal white--text"
+            style="height: 40%;"
+          >
+            <h1 class="textt">
+               {{quote}}
+            </h1>
+          </div>
+        </v-expand-transition>
     </v-carousel-item>
   </v-carousel>
-  
-  <!---- Cool Animation ------------->
+  </v-hover>
+  <!---- Cool Animation ------------>
 
   </v-layout>
 </template>
@@ -31,34 +79,71 @@ export default {
   },
   data() {
     return {
-      colors: [
-          'indigo',
-          'warning',
-          'pink darken-2',
-          'red lighten-1',
-        ],
         slides: [
           'First',
           'Second',
           'Third',
           'Fourth',
         ],
-        items: [
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-          },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
-          },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
-          },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
-          },
+        file:[
+          require('../assets/accueil_1.jpg'),
+          require('../assets/accueil_2.jpg'),
+          require('../assets/accueil_3.jpg'),
+          require('../assets/accueil_4.jpg'),
         ],
- 
+        quotes:[
+          "Inscrivez-vous gratuitement",
+          "Chercher et sélectionner des tâches",
+          "Atteindre le montant necessaire , en effectuant des tâches",
+          "Encaisser",
+        ],
+        hoverr:true,
+        inset:false,
     }
   },
 }
 </script>
+<style scoped>
+.v-card--reveal {
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  opacity: .5;
+  position: absolute;
+  width: 100%;
+}
+.textt{
+    font-size: 56px;
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+    text-align: center;
+    width: 90%;
+}
+.qu{
+  font-size: 24px;
+  font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+  text-transform: uppercase;
+  opacity: 1; 
+}
+.link {
+  position: relative;
+  text-decoration: none;
+}
+
+.link::before {
+  content: "";
+  position: absolute;
+  margin-bottom: -8px;
+  width: 100%;
+  height: 5px;
+  bottom: 0;
+  left: 0;
+  background-color: #00adb5;
+  visibility: hidden;
+  transform: scaleX(0);
+  transition: all 0.3s ease-in-out 0s;
+}
+.link:hover::before {
+  visibility: visible;
+  transform: scaleX(1); 
+}
+</style>
