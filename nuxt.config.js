104fe29b302c2628816fe62d8f547295d6d8bbbe
MostefaMourad <hm_mostefa@esi.dz>
2020-08-04 05:48:36 +0100

    deploy 2

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,21 +5,16 @@ export default {
   ** Nuxt rendering mode
   ** See https://nuxtjs.org/api/configuration-mode
   */
- /* mode: 'universal', */
-  mode:'spa',
+  mode: 'universal',
   /*
   ** Nuxt target
   ** See https://nuxtjs.org/api/configuration-target
   */
-  /*target: 'server', */
-  target:'static',
+  target: 'server',
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
   */
-  router: {
-    base: '/MoudFrontEnd/'
-  }, 
   head: {
     titleTemplate: '%s - ' + process.env.npm_package_name,
     title: process.env.npm_package_name || '',
